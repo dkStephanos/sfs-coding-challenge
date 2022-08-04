eb6964beee255d619294845ff559b3ff70177f4d
dkStephanos <dkstephanos@gmail.com>
2022-08-04 12:13:17 -0400

    correcting history hook ref

--- my-app/src/components/blogDetails.jsx
+++ my-app/src/components/blogDetails.jsx
@@ -1,16 +1,16 @@
-import { useParams, useHistory } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import useFetch from '../hooks/useFetch';
 
 const BlogDetails = () => {
 	const { id } = useParams();
 	const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
-	const history = useHistory();
+	const navigate = useNavigate();
 
 	const handleClick = () => {
 		fetch('http://localhost:8000/blogs/' + blog.id, {
 			method: 'DELETE',
 		}).then(() => {
-			history.push('/');
+			navigate.push('/');
 		});
 	};
 
