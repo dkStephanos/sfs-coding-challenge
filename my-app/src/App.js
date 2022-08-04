68cddaf1a5e916c2d1e976f45e0dd359ae805d14
dkStephanos <dkstephanos@gmail.com>
2022-08-04 12:13:31 -0400

    fixing routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,18 +12,10 @@ function App() {
 				<Navbar />
 				<div className='content'>
 					<Routes>
-						<Route exact path='/'>
-							<Home />
-						</Route>
-						<Route path='/create'>
-							<Create />
-						</Route>
-						<Route path='/blogs/:id'>
-							<BlogDetails />
-						</Route>
-						<Route path='*'>
-							<NotFound />
-						</Route>
+						<Route exact path='/' element={<Home />} />
+						<Route path='/create' element={<Create />} />
+						<Route path='/blogs/:id' element={<BlogDetails />} />
+						<Route path='*' element={<NotFound />} />
 					</Routes>
 				</div>
 			</div>
