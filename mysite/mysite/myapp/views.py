88320fa0dc74d3560542416da361b387236b18a7
dkStephanos <29663585+dkStephanos@users.noreply.github.com>
2022-08-04 17:18:27 -0400

    adding default imports and data path

--- mysite/mysite/myapp/views.py
+++ mysite/mysite/myapp/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+import os
+import pandas as pd
+from django.views import View
+from django.http import HttpResponse
 
-# Create your views here.
+DATA_PATH = os.path.join(os.path.dirname(__file__), 'data/Income Dirty Data.csv')
